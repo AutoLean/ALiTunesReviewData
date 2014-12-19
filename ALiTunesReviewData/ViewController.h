b49c477c0182ed770d1ebe1fbb1790760bf4a17f
Jeffrey Jackson <jj@autolean.com>
2014-12-19 13:56:40 -0500

    Initial Commit

---
+++ ALiTunesReviewData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALiTunesReviewData
+//
+//  Created by Jeffrey Jackson on 12/19/14.
+//  Copyright (c) 2014 AutoLean, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
